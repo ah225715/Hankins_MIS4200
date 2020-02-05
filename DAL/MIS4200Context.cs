258ffd5411605f3b4acdc169d5e12f082f3eb9a0
Alex Hankins <ah225715@ohio.edu>
2020-02-05 10:29:47 -0500

    Class Changes 2/5

--- DAL/MIS4200Context.cs
+++ DAL/MIS4200Context.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using Hankins_MIS4200.Models;
-using System.Data.Entity;
+using Hankins_MIS4200.Models;   // this is needed to access the models
+using System.Data.Entity;       // this is needed to access the DBContext
 
 namespace Hankins_MIS4200.DAL
 {
-    public class MIS4200Context : DbContext
+    public class MIS4200Context : DbContext   // inherits from Dbcontext
     {
         public MIS4200Context() : base("name=DefaultConnection")
         {
@@ -15,13 +15,15 @@ namespace Hankins_MIS4200.DAL
             // the base attribute says which connection string to use
         }
 
-        public System.Data.Entity.DbSet<Hankins_MIS4200.Models.Customer> Customers { get; set; }
-        public DbSet<Orders> Orders { get; set; }
         // Include each object here. The value inside <> is the name of the class,
         // the value outside should generally be the plural of the class name
         // and is the name used to reference the entity in code
 
-        
+       // public System.Data.Entity.DbSet<Hankins_MIS4200.Models.Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
     }
 }
